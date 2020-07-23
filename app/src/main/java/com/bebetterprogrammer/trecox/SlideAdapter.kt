8581bbc00b33664ccc639bb210f2c99359c3cdb8
Meet Parmar <meetkumar.k.parmar@gmail.com>
2020-07-23 20:32:28 +0530

    Add onBoarding screen

---
+++ app/src/main/java/com/bebetterprogrammer/trecox/SlideAdapter.kt
@@ -0,0 +1,54 @@
+package com.bebetterprogrammer.trecox
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.viewpager.widget.PagerAdapter
+import kotlinx.android.synthetic.main.slide_layout.view.*
+
+class SlideAdapter(private val context: Context) : PagerAdapter() {
+
+    var slideImage = arrayOf(
+        R.color.orange,
+        R.color.colorPrimary,
+        R.color.green
+    )
+
+    var slideTitle = arrayOf(
+        "Title 1",
+        "Title 2",
+        "Title 3"
+    )
+
+    var slideDescription = arrayOf(
+        "Description 1",
+        "Description 2",
+        "Description 3"
+    )
+
+    override fun isViewFromObject(view: View, obj: Any): Boolean {
+        return view == obj as ConstraintLayout
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val layoutInflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = layoutInflater.inflate(R.layout.slide_layout, container, false)
+
+        view.vp_image.setImageResource(slideImage[position])
+        view.vp_title.text = slideTitle[position]
+        view.vp_description.text = slideDescription[position]
+
+        container.addView(view)
+
+        return view
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, obj: Any) {
+        container.removeView(obj as ConstraintLayout?)
+    }
+
+    override fun getCount(): Int = slideTitle.size
+}

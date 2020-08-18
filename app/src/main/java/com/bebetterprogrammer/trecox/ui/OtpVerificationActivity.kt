5a6d52e83e24e699e69c9776ab8a9e6b9d8fe73d
Meet Parmar <meetkumar.k.parmar@gmail.com>
2020-08-18 11:34:11 +0530

    Create bottom navigation in Home activity

--- app/src/main/java/com/bebetterprogrammer/trecox/ui/OtpVerificationActivity.kt
+++ app/src/main/java/com/bebetterprogrammer/trecox/ui/OtpVerificationActivity.kt
@@ -1,4 +1,4 @@
-package com.bebetterprogrammer.trecox
+package com.bebetterprogrammer.trecox.ui
 
 import android.content.Intent
 import android.os.Bundle
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bebetterprogrammer.trecox.R
 import com.google.firebase.FirebaseException
 import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.FirebaseAuth

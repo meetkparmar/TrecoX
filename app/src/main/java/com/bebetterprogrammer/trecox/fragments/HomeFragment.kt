977a1bb05d264fbef97c40e55d3a7843e82c090b
hap@28 <harshitparikh2000@gmail.com>
2020-08-21 16:49:21 +0530

    mysql code added

--- app/src/main/java/com/bebetterprogrammer/trecox/fragments/HomeFragment.kt
+++ app/src/main/java/com/bebetterprogrammer/trecox/fragments/HomeFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bebetterprogrammer.trecox.R
+import com.bebetterprogrammer.trecox.networking.DataBaseConnection
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment: Fragment() {
 
@@ -14,6 +16,7 @@ class HomeFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        company_tv.text = DataBaseConnection.getName()
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
--- app/src/main/java/com/bebetterprogrammer/trecox/networking/DataBaseConnection.java
+++ app/src/main/java/com/bebetterprogrammer/trecox/networking/DataBaseConnection.java
@@ -1,8 +1,30 @@
 package com.bebetterprogrammer.trecox.networking;
 
-public class Connection {
-    String user = "epiz_26523431";
-    String password = "um4i5ui1CnI";
-    String url = "jdbc:mysql://185.27.134.3/dbName";
-    String classs = "com.mysql.jdbc.Driver"; //185.27.134.3
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DataBaseConnection {
+    public static String user = "epiz_26523431";
+    public static String password = "um4i5ui1CnI";
+    public static String url = "jdbc:mysql://185.27.134.3/epiz_26523431_trecox";
+    public static String classs = "com.mysql.jdbc.Driver"; //185.27.134.3
+
+    public static String getName() {
+        String companyEmail = "hap";
+        try {
+            Class.forName(classs);
+            Connection connection = DriverManager.getConnection(url, user, password);
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery("select * from company where company_name = 'AnalogX'");
+            companyEmail = rs.getString(3);
+            if (companyEmail == null) {
+                companyEmail = "qwe";
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return companyEmail;
+    }
 }

688cf5d8574c4ac4636e8323d49d2d2979611e9f
hap@28 <harshitparikh2000@gmail.com>
2020-08-21 16:48:36 +0530

    connection code added

---
+++ app/src/main/java/com/bebetterprogrammer/trecox/networking/DataBaseConnection.java
@@ -0,0 +1,8 @@
+package com.bebetterprogrammer.trecox.networking;
+
+public class Connection {
+    String user = "epiz_26523431";
+    String password = "um4i5ui1CnI";
+    String url = "jdbc:mysql://185.27.134.3/dbName";
+    String classs = "com.mysql.jdbc.Driver"; //185.27.134.3
+}
